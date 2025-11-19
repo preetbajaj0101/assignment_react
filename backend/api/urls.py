1d10f635b7555b30260bc64cf88a52766b4262bb
preetbajaj0101 <preetbajaj200@gmail.com>
2025-11-19 12:06:02 +0530

    second

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('upload/', views.upload_excel),
     path('query/', views.query_area),                          # GET: health check
+    path("compare/", views.compare_areas),
 ]
