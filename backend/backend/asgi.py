dd58d5e360bad1db2d31666260a8d1d17b4af340
preetbajaj0101 <preetbajaj200@gmail.com>
2025-11-20 11:23:18 +0530

    fix: added vercel file for deployment

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+settings_module = 'backend.deployments_settings' if os.environ.get('RENDER') else 'backend.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
